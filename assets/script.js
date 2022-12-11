ca8e2d3967c3a1a70148b6796808b1506f3d0622
Thomas Newnum <thomas.newnum@gmail.com>
2022-12-11 08:10:03 -0500

    functions are working!

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,91 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 var password = document.querySelector("#password");
-var special = "!@#$%^&*".split("")
-var number = "1234567890".split("")
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-var lower = "abcdefghijklmnopqrstuvwxyz".split("")
+var special = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var randomPassword = "";
+var ranSpecialVar ;
+var ranNumberVar ;
+var ranUpperVar ;
+var ranLowerVar ;
 
 
 
 function generatePassword() {
   console.log("button clicked")
 
-  var box0 = window.prompt("How many Characters would you like to use? min:8 max:128")
-  var box1 = window.confirm("Do you want to use special characters?")
-  var box2 = window.confirm("Do you want to use numbers?")
-  var box3 = window.confirm("Do you want to use UPPER CASE letters?")
-  var box4 = window.confirm("Do you want to use lower case letters?")
-  var passwordLength = [box0]
-  var passwordLength = [box0]
+    
+//Ask user questions and get responses
+  var box0 = window.prompt("How many Characters would you like to use? min:8 max:128");
 
+  if (!box0) {
+    alert("Please Enter Password Length");
+    
+  }else if (box0 <8 || box0 > 128) {
+    alert("Please choose a number bewteen 8 and 128");
 
-  if (box0>7 && box0<129) { 
-    console.log("TRUE")
-  }
-  else {
-    alert("!!YOUR PASSWORD SHOULD BE BETWEEN 8 and 128 characters!!")
-  }
+  }else{
+    var box1 = window.confirm("Do you want to use special characters?");
+    var box2 = window.confirm("Do you want to use numbers?");
+    var box3 = window.confirm("Do you want to use UPPER CASE letters?");
+    var box4 = window.confirm("Do you want to use lower case letters?");
+  };
   
-  for (var i = 0; i <= passwordLength; i++){
-  var randomNumber = Math.floor(Math.random() * special.length + number.length + upper.length + lower.length);
 
-  password += (randomNumber, randomNumber)
+  //picks a random character from the string being used
+  function ranSpecial() {
+     ranSpecialVar = special[Math.floor(Math.random() * special.length)]
+    return ranSpecialVar;
+    
+  }
+  ranSpecial();
+  console.log(ranSpecialVar);
+
+
+  function ranNumber() {
+    ranNumberVar = number[Math.floor(Math.random() * number.length)]
+    return ranNumberVar;
 
   }
- 
-  return password += (randomNumber, randomNumber);
+  ranNumber()
+  console.log(ranNumberVar)
+
+
+  function ranUpper() {
+    ranUpperVar = upper[Math.floor(Math.random() * upper.length)]
+    return ranUpperVar;
+  }
+
+  ranUpper()
+  console.log(ranUpperVar)
+
+  function ranLower() {
+    ranLowerVar = lower[Math.floor(Math.random() * lower.length)]
+    return ranLowerVar;
+  }
+
+  ranLower()
+  console.log(ranLowerVar)
+
+//Uses the above random characters and puts them into an array
+/*
+var randomChar = [ranSpecial(), ranNumber(), ranUpper(), ranLower()];
+  
+//loops that the above varabile to make password
+  for (var i = 0; i <= box0; i++){
+
+  var randomPasswordChar = randomChar[Math.floor(Math.random() * randomChar.length)]; 
+   randomPassword = randomPassword + randomPasswordChar;
+
+  };
+ */
+  return randomPassword;
 
 
  
-}
+};
 
 
   //for loop for each random character 
